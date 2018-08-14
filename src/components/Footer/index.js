b1ba3adca6208b054a6d462620e92d38b4ecbfc1
baicx <13522668262@163.com>
2018-08-14 15:20:21 +0800

    dev second test commit

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,7 +5,7 @@ export default class Footer extends Component {
     render() {
         return (
             <div className="footer">
-                版权所有:bcx 20180814
+                版权所有:bcx 20180814 learing react
             </div>
         );
     }
